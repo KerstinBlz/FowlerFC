be687f5262be7da7ed10b507a54cea5c42a86839
Keb <kerstin-buchholz-gar@web.de>
2015-04-28 10:39:37 +0200

    move RenterPoints to price

--- src/Movie.java
+++ src/Movie.java
@@ -36,19 +36,23 @@ public class Movie {
   double getCharge(int daysRented){
 	  return price.getCharge(daysRented);
   }
-    int getFrequentRenterPoints(int daysRented){
-    	if((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-    		return 2;
-    	else
-    		return 1;
-    }
-	
+ 	
+  int getFrequentRenterPoints(int daysRented) {
+	  return price.getFrequentRenterPoints(daysRented);
+	  }
 }
 
 abstract class Price 
 {
 	abstract int getPriceCode();
 	abstract double getCharge(int daysRented);
+	
+   int getFrequentRenterPoints(int daysRented){
+    	if((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+    		return 2;
+    	else
+    		return 1;
+    }
 
 }
 
