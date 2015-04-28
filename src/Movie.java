505cf816f1f1bbfa6124e69ede9025f6304633df
Keb <kerstin-buchholz-gar@web.de>
2015-04-28 10:35:21 +0200

    Replace Conditional with Polymorphism

--- src/Movie.java
+++ src/Movie.java
@@ -5,73 +5,88 @@ public class Movie {
     private String title;
     
     private int priceCode;
+    private Price price;
     
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         setPriceCode(priceCode);       
     }
-    public int getPriceCode() {
-        return priceCode;
+    public int getPriceCode() {    	
+        return price.getPriceCode();
     }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
+    public void setPriceCode(int arg) {    	
+    	switch (arg) {
+    	case REGULAR:
+    	price = new RegularPrice();
+    	break;
+    	case CHILDRENS:
+    	price = new ChildrensPrice();
+    	break;
+    	case NEW_RELEASE:
+    	price = new NewReleasePrice();
+    	break;
+    	default:
+    	throw new IllegalArgumentException("Incorrect Price Code");
+    	}
     }
     public String getTitle (){
         return title;
     };
     
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+  double getCharge(int daysRented){
+	  return price.getCharge(daysRented);
+  }
     int getFrequentRenterPoints(int daysRented){
     	if((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
     		return 2;
     	else
     		return 1;
     }
+	
+}
 
-    abstract class Price 
-    {
-    	abstract int getPriceCode();
-    }
-    
-	class ChildrensPrice extends Price 
+abstract class Price 
+{
+	abstract int getPriceCode();
+	abstract double getCharge(int daysRented);
+
+}
+
+class ChildrensPrice extends Price 
+{
+	int getPriceCode() 
 	{
-		int getPriceCode() 
-		{
-			return Movie.CHILDRENS;
-		}
+		return Movie.CHILDRENS;
 	}
-	
-	class NewReleasePrice extends Price 
-	{
-		int getPriceCode() 
-		{
-			return Movie.NEW_RELEASE;
+	double getCharge(int daysRented){
+		double result = 1.5;
+		if (daysRented > 3)
+		result += (daysRented - 3) * 1.5;
+		return result;
 		}
+}
+
+class NewReleasePrice extends Price 
+{
+	int getPriceCode() 
+	{
+		return Movie.NEW_RELEASE;
 	}
-	
-	class RegularPrice extends Price 
-	{
-		int getPriceCode() 
-		{
-			return Movie.REGULAR;
+	double getCharge(int daysRented){
+		return daysRented * 3;
 		}
+}
+
+class RegularPrice extends Price 
+{
+	int getPriceCode() 
+	{
+		return Movie.REGULAR;
+	}
+	double getCharge(int daysRented){
+		double result = 2;
+		if (daysRented > 2)
+		result += (daysRented - 2) * 1.5;
+		return result;
 	}
 }
